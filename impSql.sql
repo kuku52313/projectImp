3a02a651af61710f496c69a671ce2ceda7224957
kuku52313 <74461921+kuku52313@users.noreply.github.com>
2021-01-26 00:17:30 +0900

    Update impSql.sql
    sql업데이트

--- impSql.sql
+++ impSql.sql
@@ -12,6 +12,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE USER 'imp'@'localhost' IDENTIFIED BY 'root';
+grant all privileges on imp.* to 'imp'@'localhost';
 
 -- imp 데이터베이스 구조 내보내기
 CREATE DATABASE IF NOT EXISTS `imp` /*!40100 DEFAULT CHARACTER SET utf8 */;
